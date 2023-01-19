5d32f546dda327505f966243d6783ac54fd6f967
Leonamin <51023129+Leonamin@users.noreply.github.com>
2023-01-19 23:19:06 +0900

    수정 - DataSource의 역할 변경

--- lib/data/datasource/remote/tmdb_remote_datasource.dart
+++ lib/data/datasource/remote/tmdb_remote_datasource.dart
@@ -2,14 +2,18 @@ import 'dart:convert';
 
 import 'package:http/http.dart';
 import 'package:simple_movie_list_with_getx/data/dto/movie_dto.dart';
-import 'package:simple_movie_list_with_getx/domain/resoures/data_result.dart';
 
 class TmdbRemoteDataSource {
   final apiQueryParam = {
     'api_key': 'YOUR_KEY',
   };
 
-  Future<DataResult<List<MovieDTO>>> loadMovies() async {
+  // 응답에 대한 판단은 repository에서 하는게 바람직해보인다.
+  // DataSource의 역할은 데이터를 주는것에 한정하고 판단은 하지 않는것이 유연성이 있어보인다.
+  // 아니면 HTTP 응답은 HTTPResult를 따로 만들거나 라이브러리에 있는 Reponse를 받아서 status code, body를 보면 된다고 생각한다.
+  // 그러니 성공 -> 데이터,
+  // 실패 -> 예외 발생 (추후 API 설명에 나온 에러에 따라 예외처리를 일으키는게 나아보이지 않을까)
+  Future<List<MovieDTO>> loadMovies() async {
     final Uri uri =
         Uri.https('api.themoviedb.org', '/3/movie/popular', apiQueryParam);
     final Response response = await get(uri);
@@ -18,14 +22,11 @@ class TmdbRemoteDataSource {
       final Map<String, dynamic> body = json.decode(response.body);
       if (body["results"] != null) {
         final List<dynamic> results = body["results"];
-        final data =
-            results.map<MovieDTO>((item) => MovieDTO.fromJson(item)).toList();
-        return DataResult(isSuccess: true, data: data);
+        return results
+            .map<MovieDTO>((item) => MovieDTO.fromJson(item))
+            .toList();
       }
     }
-    return DataResult(
-      isSuccess: false,
-      messeage: response.body,
-    );
+    throw Exception();
   }
 }
