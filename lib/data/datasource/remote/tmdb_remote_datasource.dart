5acb1d8a51f29eeca376a9acce76daf2a08d9580
Leonamin <51023129+Leonamin@users.noreply.github.com>
2023-01-21 20:44:00 +0900

    추가 - dart define 추가

--- lib/data/datasource/remote/tmdb_remote_datasource.dart
+++ lib/data/datasource/remote/tmdb_remote_datasource.dart
@@ -4,8 +4,10 @@ import 'package:http/http.dart';
 import 'package:simple_movie_list_with_getx/data/dto/movie_dto.dart';
 
 class TmdbRemoteDataSource {
+  // 터미널에서 실행시 --dart-define TMDB_KEY=YOUR_KEY
+  // 혹은 .vscode/launch.json에서 실행 설정에 추가
   final apiQueryParam = {
-    'api_key': 'YOUR_KEY',
+    'api_key': const String.fromEnvironment('TMDB_KEY'),
   };
 
   // 응답에 대한 판단은 repository에서 하는게 바람직해보인다.
